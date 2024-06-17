cef731e3d4059cfb5bdcdd6fc9b4fbec6aac28d7
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-06-17 11:17:31 -0300

    feat: Finaliza crud das tarefas

--- API/app/Controllers/Http/TasksController.ts
+++ API/app/Controllers/Http/TasksController.ts
@@ -1,13 +1,40 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import Task from "App/Models/Task";
 
 export default class TasksController {
-  public async index({}: HttpContextContract) {}
+  public async index({}: HttpContextContract) {
+    const task = await Task.all();
+    return task;
+  }
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request }: HttpContextContract) {
+    const body = request.only(["title", "description"]);
+    const task = await Task.create({
+      title: body.title,
+      description: body.description,
+    });
 
-  public async show({}: HttpContextContract) {}
+    console.log(task.$isPersisted);
+    return task;
+  }
 
-  public async update({}: HttpContextContract) {}
+  public async show({ request }: HttpContextContract) {
+    const taskId = request.param("id");
+    const task = await Task.findOrFail(taskId);
+    return task;
+  }
 
-  public async destroy({}: HttpContextContract) {}
+  public async update({ request }: HttpContextContract) {
+    const taskId = request.param("id");
+    const body = request.only(["title", "description"]);
+    const task = await Task.findOrFail(taskId);
+    await task.merge(body).save();
+    return task;
+  }
+
+  public async destroy({ request }: HttpContextContract) {
+    const taskId = request.param("id");
+    const task = await Task.findOrFail(taskId);
+    await task.delete();
+  }
 }
